a1d56d1f269b7b1670f360dd8040c761ad9c8ed2
sinkboy-chen <129042729+sinkboy-chen@users.noreply.github.com>
2024-09-07 23:41:32 +0800

    add delete button, no delete logic yet

--- html/changeForm.js
+++ html/changeForm.js
@@ -2,3 +2,7 @@ function addNewForm() {
     localStorage.setItem('editIndex', "null");
     window.location.href = 'foodForm.html';
 }
+
+function deleteItem(index) {
+    console.log("delete");
+}
--- html/generateFoodList.js
+++ html/generateFoodList.js
@@ -13,6 +13,7 @@ function displayItems() {
         card.innerHTML = `
         <div class="card"; style="cursor: pointer;" onclick="editItem(${index})"> 
         <div class="offer-badge">500m</div>
+        <div class="top-right-badge">數量: ${item.quantity} ${item.unit}</div>
         <img src="${item.image}" onerror="this.onerror=null"; class="food-image";>
         <div class="card-content" >
             <div class="title-section">
@@ -23,7 +24,7 @@ function displayItems() {
                     <p>台灣大學男一舍</p>
                     <p>發布時間 9/7 18:06</p>
                 </div>
-                <span class="category">數量: ${item.quantity} ${item.unit}</span>
+                <button class="delete-button" onclick="deleteItem(${index}); event.stopPropagation();">DELETE</button> <!-- Delete Button -->
             </div>
         </div>
         </div>
@@ -31,12 +32,6 @@ function displayItems() {
         itemList.appendChild(card);
     });
 }
-
-// // Redirect to foodForum.html when adding a new item
-// document.getElementById('add-item').addEventListener('click', () => {
-//     localStorage.removeItem('editIndex');
-//     window.location.href = 'foodForm.html';
-// });
 
 // Edit item
 function editItem(index) {
